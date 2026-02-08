6072f5649e4b37066f39412b53afb8b90b7f6a57
Khrysoberyl <77917024+Khrysoberyl@users.noreply.github.com>
2026-02-08 17:48:07 -0500

    Update AQAnalyzer.py
    Added support for 8-16 bit clips
    Optimized speed, now the script should run 30-40 fps for 1080p clips
    Reconstructed the code, remove python loops and use vectorized numpy array calculations

--- AQAnalyzer.py
+++ AQAnalyzer.py
@@ -5,119 +5,98 @@ import mvsfunc as mvf
 import vapoursynth as vs
 core = vs.core
 
-def padimage_plane(plane, chroma=0):
-    #chroma = 0 -> 444 and for all Y planes
-    #chroma = 1 -> 420 for Chroma
-    height = plane.shape[0]
-    width = plane.shape[1]
-    if(chroma==0):
-        if(height%16==0):
-            height_padded = 0
-        else:
-            height_padded = int(np.ceil(height/16)*16-height)
-        if(width%16==0):
-            width_padded = 0
-        else:
-            width_padded = int(np.ceil(width/16)*16-width)
-        padded = plane.copy()
-        padded = np.pad(padded, ((0, height_padded), (0, width_padded)), mode = "edge")
-    if(chroma==1):
-        if(height%8==0):
-            height_padded = 0
-        else:
-            height_padded = int(np.ceil(height/8)*8-height)
-        if(width%8==0):
-            width_padded = 0
-        else:
-            width_padded = int(np.ceil(width/8)*8-width)
-        padded = plane.copy()
-        padded = np.pad(padded, ((0, height_padded), (0, width_padded)), mode = "edge")
-    
-    return padded
+def build_bwr_LUT(n=256):
+    steps = np.linspace(0.0,1.0,n, dtype=np.float32)
+    lut = np.empty((n,3), dtype=np.float32)
+    mask = steps < 0.5
 
-def MB_list_generate(padded_planes, chroma=0):
-    #padded_planes = [y, u, v]
-    #chroma = 0 -> 444 and for all Y planes
-    #chroma = 1 -> 420 for Chroma
-    MB_y_num = int(padded_planes[0].shape[0]/16)
-    MB_x_num = int(padded_planes[0].shape[1]/16)
-    MB_index_mat = np.zeros((MB_y_num, MB_x_num), dtype="int64")
-    MB_dict = {}
-    if(chroma==0):
-        for y_index in range(MB_y_num):
-            for x_index in range(MB_x_num):
-                total_index = int(y_index*1000+x_index)
-                MB_index_mat[y_index, x_index] = total_index
-                MB_dict[total_index] = [padded_planes[0][y_index*16:(y_index+1)*16, x_index*16:(x_index+1)*16].copy().astype("int64"),\
-                                        padded_planes[1][y_index*16:(y_index+1)*16, x_index*16:(x_index+1)*16].copy().astype("int64"),\
-                                        padded_planes[2][y_index*16:(y_index+1)*16, x_index*16:(x_index+1)*16].copy().astype("int64")]
+    lut[:,0] = np.where(mask, 2*steps, 1.0)          # R
+    lut[:,1] = np.where(mask, 2*steps, 2*(1-steps))  # G
+    lut[:,2] = np.where(mask, 1.0, 2*(1-steps))      # B
+
+    return (lut * (n-1)).astype(np.uint8)
+
+BWR_LUT = build_bwr_LUT(256)
+
+def padimage_plane(plane, blocksize = 16):
+    #
+    height, width = plane.shape
+    height_padded = (-height)%blocksize
+    width_padded = (-width)%blocksize
+    if((height_padded==0) and (width_padded==0)):
+        return plane
     else:
-        for y_index in range(MB_y_num):
-            for x_index in range(MB_x_num):
-                total_index = int(y_index*1000+x_index)
-                MB_index_mat[y_index, x_index] = total_index
-                MB_dict[total_index] = [padded_planes[0][y_index*16:(y_index+1)*16, x_index*16:(x_index+1)*16].copy().astype("int64"),\
-                                        padded_planes[1][y_index*8:(y_index+1)*8, x_index*8:(x_index+1)*8].copy().astype("int64"),\
-                                        padded_planes[2][y_index*8:(y_index+1)*8, x_index*8:(x_index+1)*8].copy().astype("int64")]
-    return MB_index_mat, MB_dict
+        return np.pad(plane, ((0, height_padded), (0, width_padded)), mode = "edge")
 
-def ac_energy(MB, planes: list = [0, 1, 2]):
+def plane_MB_generate(padded_plane, blocksize = 16):
+    #Split image into a (y, x, blocksize, blocksize) shaped MB tensor
+    height, width = padded_plane.shape
+    return padded_plane.reshape(height//blocksize, blocksize, width//blocksize, blocksize).transpose(0,2,1,3)
+
+def ac_energy_img(MB_list: list, planes: list = [0, 1, 2]):
+    #Calculate AC energy given the MB tensor, planes used for calculation can be tuned
     ac_energy = 0
     for plane in planes:
-        ac_energy_temp = np.square(MB[plane][:,:]).sum() - (MB[plane][:,:].sum())**2/MB[plane].size
-        ac_energy = ac_energy + ac_energy_temp
+        mb = MB_list[plane].astype(np.uint32, copy=False) #prevent overflow
+        normal_sum = mb.sum(axis = (-1,-2))
+        square_sum = np.square(mb).sum(axis = (-1,-2))
+        MBsize = mb.shape[-1]*mb.shape[-2]
+        ac_energy_temp = square_sum - normal_sum**2/MBsize
+        ac_energy = ac_energy + ac_energy_temp #dtype of ac_energy is float64
     return ac_energy
 
-def calculate_AC_energy_whole_img(MB_index_mat, MB_dict, planes: list = [0, 1, 2]):
-    #AC Energy for all the MBs
-    MB_AC_dict = {}
-    for y_index in range(int(MB_index_mat.shape[0])):
-        for x_index in range(int(MB_index_mat.shape[1])):
-            MB_AC_dict[MB_index_mat[y_index, x_index]] = ac_energy(MB_dict[MB_index_mat[y_index, x_index]], planes = planes)
-    return MB_AC_dict
+def plot_MB_stats(MB_stat_img, blocksize: int = 16):
+    #Plotting MB statistics map, e.g. AC energy, ΔQP, etc. into the original sized map from a downsized (small) MB image
+    return np.repeat(np.repeat(MB_stat_img, blocksize, axis=0), blocksize, axis=1)
 
-def plot_MB_stats(MB_index_mat, MB_stat_dict):
-    #Plotting MB statistics map, e.g. AC energy, ΔQP, etc
-    stats_img = np.zeros((int(MB_index_mat.shape[0]*16), int(MB_index_mat.shape[1]*16)), dtype="float64")
-    for y_index in range(int(MB_index_mat.shape[0])):
-        for x_index in range(int(MB_index_mat.shape[1])):
-            stats_img[y_index*16:(y_index+1)*16, x_index*16:(x_index+1)*16] = MB_stat_dict[MB_index_mat[y_index, x_index]]
-    return stats_img
-
-def MB_map_generate(MB_index_mat, MB_stat_dict):
-    #Same as plot_MB_stats, but not resized to full size of the frame
-    MB_map = np.zeros((int(MB_index_mat.shape[0]), int(MB_index_mat.shape[1])), dtype="float64")
-    for y_index in range(int(MB_index_mat.shape[0])):
-        for x_index in range(int(MB_index_mat.shape[1])):
-            MB_map[y_index, x_index] = MB_stat_dict[MB_index_mat[y_index, x_index]]
-    return MB_map
-
-def get_ac_list(MB_index_mat, MB_AC_dict):
+def get_ac_list(MB_AC_image):
     #Not used, only for outputing AC energy statistics
-    AC_list = []
-    for y_index in range(int(MB_index_mat.shape[0])):
-        for x_index in range(int(MB_index_mat.shape[1])):
-            AC_list.append(MB_AC_dict[MB_index_mat[y_index, x_index]])
-    return np.array(AC_list)
+    return np.flatten(MB_AC_image)
 
 def get_mapped_map(input_map, vmin, vmax):
+    #Used to render colors based on stats value
     cmap = matplotlib.colormaps.get_cmap('bwr')
     norm_obj = mcolors.Normalize(vmin=vmin, vmax=vmax)
     true_colored_map = cmap(norm_obj(input_map))
     rgb_image = (true_colored_map[:,:,0:3]*255).astype("uint8")
-    fullsize_img = np.zeros((int(rgb_image.shape[0]*16), int(rgb_image.shape[1]*16), 3), dtype="uint8")
-    for i in range(rgb_image.shape[0]):
-        for j in range(rgb_image.shape[1]):
-            fullsize_img[16*i:16*(i+1), 16*j:16*(j+1),:] = rgb_image[i,j,:]
-    return fullsize_img
+    
+    return plot_MB_stats(rgb_image, 16)
 
-def qp_maps_stats(MB_index_mat, MB_AC_dict, BIT_DEPTH: int, mode: int = 1, aq_strength: float = 1.0):
+def fast_bwr_map(input_map, vmin, vmax, lut=BWR_LUT):
+    norm = (input_map-vmin)/(vmax-vmin)
+    norm = np.clip(norm, 0.0, 1.0)
+    height, width = norm.shape
+    
+    index = (norm*255).astype("int32")
+    bwr_map = lut[index]
+    #bwr_map = np.empty((height, width, 3), dtype=np.float64)
+    #mask = norm < 0.5
+    #
+    #bwr_map[:, :,0] = np.where(mask, 2*norm, 1.0)          # R
+    #bwr_map[:, :,1] = np.where(mask, 2*norm, 2*(1-norm))   # G
+    #bwr_map[:, :,2] = np.where(mask, 1.0, 2*(1-norm))      # B
+    
+    #rgb_image = (bwr_map*255).astype("uint8")
+    return plot_MB_stats(bwr_map, 16)
+
+def plot_helper(input, mode: int = 1, vmin: float = 0.0, vmax: float = 0.0):
+    #mode = 1 used for difference maps as they normally have very small ranges, especially on the positive side
+    #mode = 2 used for normal Δqp maps, whether Δqp_adj or final Δqp.
+    #         They normally have decent positive values and very negative values, so some negative values are clamped out of the range for clarity
+    if(mode==1):
+        v = np.max([(np.abs(input.min())+np.abs(input.max()))/2,np.abs(input).max()])
+    elif(mode==2):
+        v = np.min([np.abs(input.min()),np.abs(input.max())]) #np.abs is needed because sometimes max value is also negative, eg. for pure black images
+    if(vmin!=0):
+        return get_mapped_map(input, vmin, vmax)
+    else:
+        return get_mapped_map(input, -v, v)
+
+def qp_maps_stats(MB_ac_map, BIT_DEPTH: int, mode: int = 1, aq_strength: float = 1.0):
     #Begins AQ algorithm
-    bit_depth_correction = 1 / (1 << (2*(BIT_DEPTH-8)))
+    bit_depth_correction = 1. / (1 << (2*(BIT_DEPTH-8)))
     bias_strength = aq_strength #aq-strength
     
-    
-    MB_ac_map = MB_map_generate(MB_index_mat, MB_AC_dict) #small MB map for calculation
     qp_adj_map = np.power(MB_ac_map * bit_depth_correction + 1, 0.125)
     avg_adj_raw = qp_adj_map.mean()
     avg_adj_pow2 = np.square(qp_adj_map).mean()
@@ -125,8 +104,8 @@ def qp_maps_stats(MB_index_mat, MB_AC_dict, BIT_DEPTH: int, mode: int = 1, aq_st
     #print(f"avg_adj_raw: {avg_adj_raw:.2f}, avg_adj:{avg_adj:.2f}\n")
     #AQ-mode = 1
     strength_1 = bias_strength * 1.0397
-    energy_helper_mat = np.ones(qp_adj_map.shape, dtype=qp_adj_map.dtype)
-    qp_adj_map_mode1 = strength_1 * (np.log2( np.fmax(MB_ac_map, energy_helper_mat) ) - (14.427 + 2*(BIT_DEPTH-8)))
+    #energy_helper_mat = np.ones(qp_adj_map.shape, dtype=qp_adj_map.dtype)
+    qp_adj_map_mode1 = strength_1 * (np.log2( np.fmax(MB_ac_map, 1) ) - (14.427 + 2*(BIT_DEPTH-8)))
     fin_qp_offset_map_mode1 = qp_adj_map+qp_adj_map_mode1
     
     #AQ-mode = 2
@@ -163,81 +142,39 @@ def fill_frame(n, f):
     display_stats = f[1].props["_display_stats"]
     vmin = f[1].props["_vmin"]
     vmax = f[1].props["_vmax"]
-    y = padimage_plane(np.asarray(source[0])[:],0)
+    y_MB = plane_MB_generate(padimage_plane(np.asarray(source[0])[:],16),16)
     if("YUV444" in source.format.name):
-        u = padimage_plane(np.asarray(source[1])[:],0)
-        v = padimage_plane(np.asarray(source[2])[:],0)
-        MB_index_mat, MB_dict = MB_list_generate([y, u, v], 0)
-        MB_AC_dict = calculate_AC_energy_whole_img(MB_index_mat, MB_dict, planes = [0, 1, 2])
+        u_MB = plane_MB_generate(padimage_plane(np.asarray(source[1])[:], blocksize = 16), 16)
+        v_MB = plane_MB_generate(padimage_plane(np.asarray(source[2])[:], blocksize = 16), 16)
     elif("YUV420" in source.format.name):
-        u = padimage_plane(np.asarray(source[1])[:],1)
-        v = padimage_plane(np.asarray(source[2])[:],1)
-        MB_index_mat, MB_dict = MB_list_generate([y, u, v], 1)
-        MB_AC_dict = calculate_AC_energy_whole_img(MB_index_mat, MB_dict, planes = [0, 1, 2])
+        u_MB = plane_MB_generate(padimage_plane(np.asarray(source[1])[:], blocksize = 8), 8)
+        v_MB = plane_MB_generate(padimage_plane(np.asarray(source[2])[:], blocksize = 8), 8)
+    MB_ac_map = ac_energy_img([y_MB, u_MB, v_MB], planes = [0,1,2])
     (avg_adj_raw, avg_adj), (qp_adj_map_mode1, fin_qp_offset_map_mode1), (qp_adj_map_mode2, fin_qp_offset_map_mode2), (qp_adj_map_mode3, fin_qp_offset_map_mode3),\
-    (qp_map_diff_2to1, qp_map_diff_3to1, qp_map_diff_3to2) = qp_maps_stats(MB_index_mat, MB_AC_dict, aq_strength = 1.0, BIT_DEPTH = BIT_DEPTH)
+    (qp_map_diff_2to1, qp_map_diff_3to1, qp_map_diff_3to2) = qp_maps_stats(MB_ac_map, aq_strength = 1.0, BIT_DEPTH = BIT_DEPTH)
     if(aq_mode == 0):
-        ac_map = MB_map_generate(MB_index_mat, MB_AC_dict)
-        rgb_image = get_mapped_map(ac_map, 0, ac_map.max()*0.5)
-    else:
-        if(diff==2):
-            v = np.max([(np.abs(qp_map_diff_2to1.min())+qp_map_diff_2to1.max())/2,qp_map_diff_2to1.max()])
-            if(vmin!=0):
-                rgb_image = get_mapped_map(qp_map_diff_2to1, vmin, vmax)
-            else:
-                rgb_image = get_mapped_map(qp_map_diff_2to1, -v, v)
-        elif(diff==3):
-            v = np.max([(np.abs(qp_map_diff_3to1.min())+qp_map_diff_3to1.max())/2,qp_map_diff_3to1.max()])
-            if(vmin!=0):
-                rgb_image = get_mapped_map(qp_map_diff_3to1, vmin, vmax)
-            else:
-                rgb_image = get_mapped_map(qp_map_diff_3to1, -v, v)
-        elif(diff==6):
-            v = np.max([(np.abs(qp_map_diff_3to2.min())+qp_map_diff_3to2.max())/2,qp_map_diff_3to2.max()])
-            if(vmin!=0):
-                rgb_image = get_mapped_map(qp_map_diff_3to2, vmin, vmax)
-            else:
-                rgb_image = get_mapped_map(qp_map_diff_3to2, -v, v)
+        rgb_image = get_mapped_map(MB_ac_map, 0, MB_ac_map.max()*0.5)
+    elif(diff==2):
+        rgb_image = plot_helper(qp_map_diff_2to1, mode = 1, vmin = vmin, vmax = vmax)
+    elif(diff==3):
+        rgb_image = plot_helper(qp_map_diff_3to1, mode = 1, vmin = vmin, vmax = vmax)
+    elif(diff==6):
+        rgb_image = plot_helper(qp_map_diff_3to2, mode = 1, vmin = vmin, vmax = vmax)
+    elif(aq_mode == 1):
+        if(show_fin_delta_qp_map == 1):
+            rgb_image = plot_helper(fin_qp_offset_map_mode1, mode = 2, vmin = vmin, vmax = vmax)
         else:
-            if(aq_mode == 1):
-                if(show_fin_delta_qp_map == 1):
-                    v = np.min([np.abs(fin_qp_offset_map_mode1.min()),fin_qp_offset_map_mode1.max()])
-                    if(vmin!=0):
-                        rgb_image = get_mapped_map(fin_qp_offset_map_mode1, vmin, vmax)
-                    else:
-                        rgb_image = get_mapped_map(fin_qp_offset_map_mode1, -v, v)
-                else:
-                    v = np.min([np.abs(qp_adj_map_mode1.min()),qp_adj_map_mode1.max()])
-                    if(vmin!=0):
-                        rgb_image = get_mapped_map(qp_adj_map_mode1, vmin, vmax)
-                    else:
-                        rgb_image = get_mapped_map(qp_adj_map_mode1, -v, v)
-            elif(aq_mode == 2):
-                if(show_fin_delta_qp_map == 1):
-                    v = np.min([np.abs(fin_qp_offset_map_mode2.min()),fin_qp_offset_map_mode2.max()])
-                    if(vmin!=0):
-                        rgb_image = get_mapped_map(fin_qp_offset_map_mode2, vmin, vmax)
-                    else:
-                        rgb_image = get_mapped_map(fin_qp_offset_map_mode2, -v, v)
-                else:
-                    v = np.min([np.abs(qp_adj_map_mode2.min()),qp_adj_map_mode2.max()])
-                    if(vmin!=0):
-                        rgb_image = get_mapped_map(qp_adj_map_mode2, vmin, vmax)
-                    else:
-                        rgb_image = get_mapped_map(qp_adj_map_mode2, -v, v)
-            elif(aq_mode == 3):
-                if(show_fin_delta_qp_map == 1):
-                    v = np.min([np.abs(fin_qp_offset_map_mode3.min()),fin_qp_offset_map_mode3.max()])
-                    if(vmin!=0):
-                        rgb_image = get_mapped_map(fin_qp_offset_map_mode3, vmin, vmax)
-                    else:
-                        rgb_image = get_mapped_map(fin_qp_offset_map_mode3, -v, v)
-                else:
-                    v = np.min([np.abs(qp_adj_map_mode3.min()),qp_adj_map_mode3.max()])
-                    if(vmin!=0):
-                        rgb_image = get_mapped_map(qp_adj_map_mode3, vmin, vmax)
-                    else:
-                        rgb_image = get_mapped_map(qp_adj_map_mode3, -v, v)
+            rgb_image = plot_helper(qp_adj_map_mode1, mode = 2, vmin = vmin, vmax = vmax)
+    elif(aq_mode == 2):
+        if(show_fin_delta_qp_map == 1):
+            rgb_image = plot_helper(fin_qp_offset_map_mode2, mode = 2, vmin = vmin, vmax = vmax)
+        else:
+            rgb_image = plot_helper(qp_adj_map_mode2, mode = 2, vmin = vmin, vmax = vmax)
+    elif(aq_mode == 3):
+        if(show_fin_delta_qp_map == 1):
+            rgb_image = plot_helper(fin_qp_offset_map_mode3, mode = 2, vmin = vmin, vmax = vmax)
+        else:
+            rgb_image = plot_helper(qp_adj_map_mode3, mode = 2, vmin = vmin, vmax = vmax)
     for p in range(fout.format.num_planes):
         np.asarray(fout[p])[:] = rgb_image[:fout.height,:fout.width,p]
     qamm1_min = qp_adj_map_mode1.min()
@@ -303,7 +240,8 @@ def wrapper(clip: vs.VideoNode, aq_mode: int = 1, diff: int = 0, show_fin_delta_
         css = "22"
     else:
         raise TypeError('\"clip\" must be YUV420 or YUV444!')
-    clip2 = mvf.ToRGB(clip)
+    #clip2 = mvf.ToRGB(clip)
+    clip2 = core.std.BlankClip(clip, format=vs.RGB24)
     clip2 = core.std.SetFrameProp(clip2, prop="_aqmode", intval = aq_mode)
     clip2 = core.std.SetFrameProp(clip2, prop="_diff", intval = diff)
     clip2 = core.std.SetFrameProp(clip2, prop="_finqp", intval = show_fin_delta_qp_map)
@@ -312,5 +250,5 @@ def wrapper(clip: vs.VideoNode, aq_mode: int = 1, diff: int = 0, show_fin_delta_
     clip2 = core.std.SetFrameProp(clip2, prop="_vmin", floatval = vmin)
     clip2 = core.std.SetFrameProp(clip2, prop="_vmax", floatval = vmax)
     rgb_clip = core.std.ModifyFrame(clip2, [clip, clip2], fill_frame)
-    clip_out = mvf.ToYUV(rgb_clip, full=True, css=css, matrix=matrix)
+    clip_out = mvf.ToYUV(rgb_clip, full=True, css=css, matrix=matrix, depth=clip.format.bits_per_sample)
     return clip_out
